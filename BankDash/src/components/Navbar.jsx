37df2491a0752010c1f7a92bd3cdb75865e8526d
ayomideRussianCode <ayindeayomide7@gmail.com>
2026-03-31 23:46:07 +0100

    last commit in March

--- BankDash/src/components/Navbar.jsx
+++ BankDash/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
           <div className=" flex  ">
             <div className="relative">
               <input  placeholder="Search for something" className="rounded-4xl outline-0  py-2 px-14 text-[#8ba3cb] bg-[#f5f7fa]" type="text" />
-              <img className="absolute top-3 left-4 " src="/search-icon.png" alt="" />
+              <img className="absolute top-3 left-4 " src="/search-icon.png" alt="Search" />
             </div>
             <div className="mx-6">
               <img src="/settings-icon-2.png" alt="" />
